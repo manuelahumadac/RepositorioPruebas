746094e9bcafc9b063afaccec379c2fdc1bbcfb6
manuel.ahumada.c <manuel.ahumada.c@outlook.es>
2015-03-03 15:52:44 -0400

    Primer intento de publicacion

---
+++ HolaMundoConsola/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HolaMundoConsola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Nuevo comentario");
+            Console.ReadLine();
+        }
+    }
+}
